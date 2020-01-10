7af905b5019fa473dfcd4039b3f2efe412f20d8a
Lisa Geers <lizzgeers@hotmail.com>
2020-01-10 14:55:44 +0100

    3d matplot and spaces removed

--- main.py
+++ main.py
@@ -14,51 +14,51 @@ from code.visualisation import matplot
 
 if __name__ == '__main__':
 
-    # let user choose a chip
-    while True:
-        req_chip = input("Which chip do you want to use? (1, 2, test)\n")
-        if req_chip == '1':
-            chip_path = 'data/chip_1/print_1.csv'
-            req_chip = 'chip_1'
-            break
-        elif req_chip == '2':
-            chip_path = 'data/chip_2/print_2.csv'
-            req_chip = 'chip_2'
-            break
-        elif req_chip.lower() == 'test':
-            chip_path = 'data/test/print.csv'
-            req_chip = 'test'
-            break
-        else:
-            print("That chip does not exist.")
+	# let user choose a chip
+	while True:
+		req_chip = input("Which chip do you want to use? (1, 2, test)\n")
+		if req_chip == '1':
+			chip_path = 'data/chip_1/print_1.csv'
+			req_chip = 'chip_1'
+			break
+		elif req_chip == '2':
+			chip_path = 'data/chip_2/print_2.csv'
+			req_chip = 'chip_2'
+			break
+		elif req_chip.lower() == 'test':
+			chip_path = 'data/test/print.csv'
+			req_chip = 'test'
+			break
+		else:
+			print("That chip does not exist.")
 
-    # create a Chip object for the chosen chip
-    chip = chip.Chip(chip_path)
+	# create a Chip object for the chosen chip
+	chip = chip.Chip(chip_path)
 
-    # let user choose a netlist
-    while True:
-        req_netlist = input("Which netlist do you want to use? (1, 2, 3)\n")
-        if req_netlist == '1':
-            netlist_path = 'data/' + req_chip + '/netlist_1.csv'
-            break
-        elif req_netlist == '2':
-            netlist_path = 'data/' + req_chip + '/netlist_2.csv'
-            break
-        elif req_netlist == '3':
-            netlist_path = 'data/' + req_chip + '/netlist_3.csv'
-            break
-        else:
-            print("That is not an option.")
+	# let user choose a netlist
+	while True:
+		req_netlist = input("Which netlist do you want to use? (1, 2, 3)\n")
+		if req_netlist == '1':
+			netlist_path = 'data/' + req_chip + '/netlist_1.csv'
+			break
+		elif req_netlist == '2':
+			netlist_path = 'data/' + req_chip + '/netlist_2.csv'
+			break
+		elif req_netlist == '3':
+			netlist_path = 'data/' + req_chip + '/netlist_3.csv'
+			break
+		else:
+			print("That is not an option.")
 
-    # create a Netlist object for the chosen chip and netlist combination
-    netlist = netlist.Netlist(netlist_path, chip.gates)
+	# create a Netlist object for the chosen chip and netlist combination
+	netlist = netlist.Netlist(netlist_path, chip.gates)
 
-    # generate a solution
-    wiring = wiring.Wiring(netlist, chip)
+	# generate a solution
+	wiring = wiring.Wiring(netlist, chip)
 
-    # get the dimensions for the visual representation
-    x_dim = chip.get_x_dimension(chip.gates)
-    y_dim = chip.get_y_dimension(chip.gates)
+	# get the dimensions for the visual representation
+	x_dim = chip.get_x_dimension(chip.gates)
+	y_dim = chip.get_y_dimension(chip.gates)
 
-    # create visual representation of the solved chip
-    visualise = matplot.visualise(chip.gates, wiring.wire, x_dim, y_dim)
+	# create visual representation of the solved chip
+	visualise = matplot.visualise(chip.gates, wiring.wire, x_dim, y_dim)
