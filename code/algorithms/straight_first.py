44f56039fa3557c08e9f951a95cb297d5c6b774f
Marte <marte_vd_wijk@hotmail.com>
2020-01-13 10:42:57 +0100

    random including z

--- code/algorithms/straight_first.py
+++ code/algorithms/straight_first.py
@@ -30,35 +30,49 @@ def straight_wire(net_cor, chip):
         # move while not at end
         while True:
             # check whether current point and end point are adjacent
-            if (abs(current_cor[0] - end_cor[0]) == 1 and current_cor[1] - end_cor[1] == 0) or (abs(current_cor[1] - end_cor[1]) == 1 and current_cor[0] - end_cor[0] == 0):
+            if abs(current_cor[0] - end_cor[0]) + abs(current_cor[1] - end_cor[1]) + abs(current_cor[2] - end_cor[2]) == 1:
                 wire.append(net[1])
                 output_dict[net] = wire
                 break
 
             # move towards the end-gate
             else:
-                # move +x if x of end is larger and +x is open
-                if (end_cor[0] - current_cor[0]) > 0 and chip.check_empty(((current_cor[0] + 1), current_cor[1]), chip.grid):
+            # move +x if x of end is larger and +x is open
+                if (end_cor[0] - current_cor[0]) > 0 and chip.check_empty(((current_cor[0] + 1), current_cor[1], current_cor[2]), chip.grid):
                     current_cor[0] += 1
-                    chip.grid[current_cor[0]][current_cor[1]] = True
+                    chip.grid[current_cor[0]][current_cor[1]][current_cor[2]] = True
                     wire.append(tuple(current_cor))
 
                 # move -x if x of end is smaller and -x is open
-                elif ((end_cor[0] - current_cor[0]) < 0) and chip.check_empty(((current_cor[0] - 1), current_cor[1]), chip.grid):
+                elif ((end_cor[0] - current_cor[0]) < 0) and chip.check_empty(((current_cor[0] - 1), current_cor[1], current_cor[2]), chip.grid):
                     current_cor[0] -= 1
-                    chip.grid[current_cor[0]][current_cor[1]] = True
+                    chip.grid[current_cor[0]][current_cor[1]][current_cor[2]] = True
                     wire.append(tuple(current_cor))
 
                 # move +y if y of end is larger and +y is open
-                elif ((end_cor[1] - current_cor[1]) > 0) and chip.check_empty((current_cor[0], (current_cor[1] + 1)), chip.grid):
+                elif ((end_cor[1] - current_cor[1]) > 0) and chip.check_empty((current_cor[0], (current_cor[1] + 1), current_cor[2]), chip.grid):
                     current_cor[1] += 1
-                    chip.grid[current_cor[0]][current_cor[1]] = True
+                    chip.grid[current_cor[0]][current_cor[1]][current_cor[2]] = True
                     wire.append(tuple(current_cor))
 
                 # move -y if y of end is smaller and -y is open
-                elif ((end_cor[1] - current_cor[1]) < 0) and chip.check_empty((current_cor[0], (current_cor[1] - 1)), chip.grid):
+                elif ((end_cor[1] - current_cor[1]) < 0) and chip.check_empty((current_cor[0], (current_cor[1] - 1), current_cor[2]), chip.grid):
                     current_cor[1] -= 1
-                    chip.grid[current_cor[0]][current_cor[1]] = True
+                    chip.grid[current_cor[0]][current_cor[1]][current_cor[2]] = True
                     wire.append(tuple(current_cor))
 
-    return output_dict
+                # move -z if z of end is larger and -z is open
+                elif ((end_cor[2] - current_cor[2]) < 0) and chip.check_empty((current_cor[0], current_cor[1], (current_cor[2] - 1)), chip.grid):
+                    current_cor[2] -= 1
+                    chip.grid[current_cor[0]][current_cor[1]][current_cor[2]] = True
+                    wire.append(tuple(current_cor))
+
+                # move +z if z of end is larger and +z is open
+                else:
+                    if current_cor[2] + 1 == 8:
+                        return None
+                    current_cor[2] += 1
+                    chip.grid[current_cor[0]][current_cor[1]][current_cor[2]] = True
+                    wire.append(tuple(current_cor))
+
+        return output_dict
