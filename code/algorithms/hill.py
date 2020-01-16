543c7353cb2b5d6cc964669cc4e039c021b0172c
Marte <marte_vd_wijk@hotmail.com>
2020-01-16 14:28:21 +0100

    start hill

---
+++ code/algorithms/hill.py
@@ -0,0 +1,12 @@
+import copy
+
+class HillClimber():
+	def __init__(self, grid, netlist):
+		if not grid.is_solution():
+			raise Exception("HillClimber requires a complete solution.")
+
+		self.grid = copy.deepcopy(grid)
+		
+
+		self.netlist = netlist
+	
