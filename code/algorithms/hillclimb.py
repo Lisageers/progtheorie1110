808cc9b76b984d68ea4a5b63368d37b84c13e4a9
Lisa Geers <lizzgeers@hotmail.com>
2020-01-21 16:11:29 +0100

    stats and comare

--- code/algorithms/hillclimb.py
+++ code/algorithms/hillclimb.py
@@ -1,5 +1,6 @@
 from code.algorithms.xyz_move import xyz_wire
 from code.algorithms.astar import execute_astar
+import random
 
 def find_point_stuck(output_dict, unsolved_wire):
 	""" Find the point where the wire gets stuck in the grid. """
@@ -18,17 +19,23 @@ def find_point_stuck(output_dict, unsolved_wire):
 
 				# find the point where wire gets stuck
 				if net[1][0] == point[0] and net[1][1] == point[1] and (unsolved_wire[net].count(point) == 1) or point == unsolved_wire[net][-1]:
+					#put wire and point in dictionary
 					stuck_wires[net] = wire
 					stuck_point[net] = point
 					break
 
+				# add point to list if stuck-point is not found
 				wire.append(point)
 
 	return stuck_point, stuck_wires
 
 def change_wires(stuck_points, stuck_wires, chip, output_dict):
+	""" Lays the remaining points of the wires that got stuck with the use of A*. """
 
 	new_output_dict = {}
+
+
+
 	for net, point in stuck_points.items():
 		# make new netlist
 		new_netlist = [(point, net[1])]
@@ -41,11 +48,7 @@ def change_wires(stuck_points, stuck_wires, chip, output_dict):
 			stuck_wires[net] += next(iter(new_wires.values()))
 			new_output_dict[net] = stuck_wires[net]
 
+	# update the output with new wires
 	output_dict.update(new_output_dict)
-
-	unsolved_count = 0
-
-	for net in stuck_points:
-		unsolved_count += 1
 
 	return output_dict
